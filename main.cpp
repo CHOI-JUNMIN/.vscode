1e0489b9c97e7e47c9959f5d2046c37ffcb03b85
최준민 <csm0791@naver.com>
2024-07-26 17:26:15 +0900

    20240726 Dependency파일만들기

--- main.cpp
+++ main.cpp
@@ -1,30 +1,3 @@
-/*
-#include <iostream>
-int main()
-{
-    double num1, num2;
-
-    std::cout << "첫 번째 숫자: ";
-    std::cin >> num1;
-
-    std::cout << "두 번째 숫자: ";
-    std::cin >> num2;
-
-    double sum = num1 + num2;
-
-    std::cout << "숫자 합: " << sum << std::endl;
-
-    return 0;
-}*/
-/*
-#include <iostream>
-
-int main(int argc, const char **argv)
-{
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
-}*/
-
 #include <spdlog/spdlog.h>
 
 int main(int argc, const char **argv)
